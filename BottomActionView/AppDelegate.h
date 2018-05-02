f1fdabb0a742a98685f98869522b4139f2049d1e
Ragaie alfy <Ragaie@ehabs-bc-macbook-pro.local>
2018-05-02 14:31:05 +0200

    Initial Commit

---
+++ BottomActionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BottomActionView
+//
+//  Created by Ragaie alfy on 5/2/18.
+//  Copyright © 2018 Ragaie alfy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BottomActionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BottomActionView
+//
+//  Created by Ragaie alfy on 5/2/18.
+//  Copyright © 2018 Ragaie alfy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
